16214fd8b19e98b08a21cca8a0524dbf084a24d4
dunyawayfarer <yasirsiddiqui0012@gmail.com>
2025-03-07 06:49:22 +0530

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,20 @@
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 80 },
+        color: { value: "#ffffff" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5, random: true },
+        size: { value: 3, random: true },
+        move: { enable: true, speed: 1 }
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: "repulse" },
+            onclick: { enable: true, mode: "push" }
+        },
+        modes: {
+            repulse: { distance: 100 },
+            push: { particles_nb: 4 }
+        }
+    }
+});
